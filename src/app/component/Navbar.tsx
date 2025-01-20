9440c9478607a6d5d7246697cc705d293c286f82
jaweriaawan <akhterwaheed470@gmail.com>
2025-01-21 01:51:58 +0500

    changes

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
+
 import Link from "next/link";
 import { IoBagOutline } from "react-icons/io5";
 import { useSelector } from "react-redux";
-import { RootState } from "@reduxjs/toolkit/query"; // Adjust the path to where your store is defined.
+import { RootState } from "../Redux/Store";
 
 export default function Navbar() {
-  const item = useSelector((state: RootState) => state.cart); // Accessing the cart state
+  const cartItems = useSelector((state: RootState) => state.cart); // Access the cart state directly since it's an array
 
   return (
     <div>
@@ -14,30 +15,10 @@ export default function Navbar() {
           Home
         </Link>
         <Link href="/cart" className="text-[25px] font-bold">
-          Cart ({item.length}) {/* Example: Display cart item count */}
+          Cart ({cartItems.length}) {/* Display cart item count */}
         </Link>
         <IoBagOutline className="w-[28px] h-[28px]" />
       </div>
     </div>
   );
 }
-
-
-
-// import Link from "next/link"
-// import { IoBagOutline } from "react-icons/io5";
-// import { useSelector } from "react-redux";
-// import { Provider } from "react-redux";
-
-// export default function Navbar(){
-//      <Provider>
-//          const item = useSelector((state)=> state.cart)
-//      </Provider>
-//  return <div>
-//   <div className="flex bg-slate-400 bg-opacity-20 w-full h-[100px] border-b-[3px] border-black mb-[50px] justify-around pt-[20px]">
-//      <Link href="/" className="text-[25px] font-bold">Home</Link>
-//      <Link href="/" className="text-[25px] font-bold">Cart</Link>
-//      <IoBagOutline className="w-[28px] h-[28px]" />
-//   </div>
-// </div>
-// }
